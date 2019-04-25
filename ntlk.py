36c6f23eac3459b91f26fe1f02de74602157ca07
neodeng78 <dduncan@163.com>
2019-04-25 20:07:52 +0800

    Delete ntlk.py

--- ntlk.py
+++
@@ -1,8 +0,0 @@
-import nltk
-nltk.download('punkt')
-nltk.download('gutenberg')
-nltk.download('genesis')
-nltk.download('inaugural')
-nltk.download('nps_chat')
-nltk.download('webtext')
-nltk.download('treebank')
