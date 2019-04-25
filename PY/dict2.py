0fca5f598653543c17756b147a959e17973ac5d6
neodeng78 <dduncan@163.com>
2019-04-25 20:22:58 +0800

    Create dict2.py

---
+++ PY/dict2.py
@@ -0,0 +1,31 @@
+"""
+
+字典的常用操作
+
+Neo edit
+
+"""
+
+
+def main():
+	stu = {'name': 'neo', 'age': 41, 'gender': True}
+	print(stu)
+	print(stu.keys())
+	print(stu.values())
+	print(stu.items())
+	for elem in stu.items():
+		print(elem)
+		print(elem[0], elem[1])
+	if 'age' in stu:
+		stu['age'] = 20
+	print(stu)
+	stu.setdefault('score', 60)
+	print(stu)
+	stu.setdefault('score', 100)
+	print(stu)
+	stu['score'] = 100
+	print(stu)
+
+
+if __name__ == '__main__':
+	main()
