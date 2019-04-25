abf26301e06cfaa98c0958ba0e5f083836afb356
neodeng78 <dduncan@163.com>
2019-04-25 20:04:45 +0800

    Create ntlk.py

---
+++ PY/ntlk.py
@@ -0,0 +1,8 @@
+import nltk
+nltk.download('punkt')
+nltk.download('gutenberg')
+nltk.download('genesis')
+nltk.download('inaugural')
+nltk.download('nps_chat')
+nltk.download('webtext')
+nltk.download('treebank')
