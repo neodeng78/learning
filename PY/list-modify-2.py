79c20b6c9891ccc885314a5cbc4d3625c684342b
neodeng78 <dduncan@163.com>
2019-04-30 08:29:31 +0800

    Create list-modify-2.py

---
+++ PY/list-modify-2.py
@@ -0,0 +1,12 @@
+import nester
+
+movies=[
+"the Holy Grail",1975,"Terry Jones & Terry Gilliam",91,
+["Graham Chapman",
+["michael Plain","John Cleese","Terry Gilliam","Eric Idle","Terry Jones"]]]
+
+print(movies[4][1][3])
+cast=['Paulin','Clesse','Idle','Jones',['Neo',['King','Arthe']]]
+
+nester.print_lol(movies,5)
+
