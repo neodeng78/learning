f177bb7bdd2996a02efae14decbae7f88e92b921
neodeng78 <dduncan@163.com>
2019-04-30 08:30:18 +0800

    Create test.py

---
+++ PY/test.py
@@ -0,0 +1,26 @@
+
+movies=[
+"the Holy Grail",1975,"Terry Jones & Terry Gilliam",91,
+["Graham Chapman",
+["michael Plain","John Cleese","Terry Gilliam","Eric Idle","Terry Jones"]]]
+
+#print(movies[4][1][3])
+fav_movie=[]
+for each_item in movies:
+    if isinstance(each_item,list):
+        for nested_item in each_item:
+            if isinstance(nested_item,list):
+                for deeper_item in nested_item:
+                    print(deeper_item)
+                else:
+                    print(each_item)
+
+
+            print(nested_item,end="");
+        else:
+            fav_movie.append(each_item)
+            print(each_item,end="");
+            #print(fav_movie)
+
+#print(movies)
+#print(len(movies))
